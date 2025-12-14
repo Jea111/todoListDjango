786a304d0446aa1f59f8a69ab67d9983193f8788
jea111 <laliendra42@gmail.com>
2025-12-14 16:02:33 -0500

    delete,update de tareas y update de los datos del usuario

--- tareas/urls.py
+++ tareas/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import createTarea,tareasAllViews
+from .views import createTarea,tareasAllViews,editar_tarea,eliminar_tarea,editar_usuario
 
 urlpatterns = [
-    path('',createTarea,name='crearTarea'),
-    path('tareas/',tareasAllViews,name='tareas')
+    path('crearTarea',createTarea,name='crearTarea'),
+    path('',tareasAllViews,name='tareas'),
+    path('editar_tarea/<int:id>/',editar_tarea,name='editar_tarea'),
+    path('eliminar_tarea/<int:id>/',eliminar_tarea,name='eliminar_tarea'),
+    path('editar_usuario/<int:id>/',editar_usuario,name='editar_usuario'),
 ]
